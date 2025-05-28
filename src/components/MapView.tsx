3af6a72f12542583f533ecd9601b5359b79cec84
juan_gomez9905 <juan.gomez@bebolder.co>
2025-05-28 17:37:30 -0500

    adding maptiler to show a map in user location

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -1,17 +1,39 @@
-import { useContext } from "react";
+import { useContext, useLayoutEffect, useRef } from "react";
 import { PlacesContext } from "../context";
 import { Loading } from "./Loading";
+import { Map, MapStyle } from '@maptiler/sdk';
 
 export const MapView = () => {
 
-  const { isLoading, userLocation } = useContext( PlacesContext )
+  const { isLoading, userLocation } = useContext( PlacesContext );
+  const mapDiv = useRef<HTMLDivElement>(null);
+
+  useLayoutEffect(() => {
+    if (!isLoading) {
+      const map = new Map({
+        container: mapDiv.current!, // container's id or the HTML element in which the SDK will render the map
+        style: MapStyle.STREETS,
+        center: userLocation, // starting position [lng, lat]
+        zoom: 14 // starting zoom
+      });
+    }
+  }, [ isLoading ])
 
   if (isLoading) {
     return (<Loading />);
   }
 
   return (
-    <div>
+    <div 
+      ref={ mapDiv }
+      style={{
+        height: '100vh',
+        left: 0,
+        position: 'fixed',
+        top: '0',
+        width: '100vw'
+      }}
+    >
         { userLocation?.join(',') }
     </div>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { MapsApp } from './MapsApp'
 
+import { config } from '@maptiler/sdk';
+
 if ( !navigator.geolocation ) {
   alert('Tu navegador no tiene opción de Geolocation')
   throw new Error('Tu navegador no tiene opción de Geolocation')
 }
+
+config.apiKey = 'MrGcFBAfgejdBRruVsiW';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
