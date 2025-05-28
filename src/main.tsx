63721ee03468c8bb16abe19fc4ac9d9cc3d31fb8
juan_gomez9905 <juan.gomez@bebolder.co>
2025-05-28 09:05:46 -0500

    adding places context and mapsApp component

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { MapsApp } from './MapsApp'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <MapsApp />
   </StrictMode>,
 )
