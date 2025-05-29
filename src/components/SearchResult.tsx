c2e1e28c9628ccc4ae6a2daca1080a89d81c6115
juan_gomez9905 <juan.gomez@bebolder.co>
2025-05-29 17:14:43 -0500

    adding navigation tu plae, when user click on one option y the search box

--- src/components/SearchResult.tsx
+++ src/components/SearchResult.tsx
@@ -1,9 +1,21 @@
-import { useContext } from "react";
-import { PlacesContext } from "../context";
+import { useContext, useState } from "react";
+import { MapContext, PlacesContext } from "../context";
 import { LoadingPlaces } from "./LoadingPlaces";
+import type { Feature } from "../interfaces/places";
 
 export const SearchResult = () => {
     const { places, isLoadingPlaces } = useContext(PlacesContext);
+    const { map } = useContext(MapContext);
+
+    const [activeId, setActiveId] = useState('');
+
+    const onPlaceClicked = (place: Feature) => {
+        setActiveId(place.id);
+        map?.flyTo({
+            zoom: 14,
+            center: place.center
+        });
+    }
 
     if (isLoadingPlaces) {
         return (<LoadingPlaces />);
@@ -12,10 +24,14 @@ export const SearchResult = () => {
     return (
         <ul className={`list-group mb-0 ${places.length === 0 ? '' : 'mt-3'}`}>
             { places.map((place) => (
-                <li key={place.id} className="list-group-item list-group-item-action">
+                <li
+                    onClick={ () => onPlaceClicked(place) }
+                    key={place.id}
+                    className={`list-group-item list-group-item-action cursor-pointer ${activeId === place.id ? 'active' : ''}`}
+                >
                     <h6> { place.text_es } </h6>
-                    <p className="text-muted" style={{ fontSize: '12px' }}> { place.place_name } </p>
-                    <button className="btn btn-outline-primary btn-sm">Direcciones</button>
+                    <p style={{ fontSize: '12px' }}> { place.place_name } </p>
+                    <button className={`btn btn-sm ${activeId === place.id ? 'btn-outline-light' : 'btn-outline-primary'}`}>Direcciones</button>
                 </li>
             )) }
         </ul>
